edbea8682a2e95944039746212bd30fe2108e938
Ronipaul99 <ronipaul9972@gmail.com>
2022-06-26 11:02:44 +0530

    chenge settings

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const myErrorLogger = require('./src/utilities/ErrorLogger');
@@ -6,21 +7,27 @@ const myRequestLogger = require('./src/utilities/RequestLogger');
 const userRouter = require('./src/routes/userRouter');
 const DestinationRouter = require('./src/routes/DestinationRouter');
 const BookingRouter = require('./src/routes/BookingRouter');
-const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000
+
 app.use(myRequestLogger);
 
+app.get('/',(req,res)=>{
+    res.send("Welcome to Tripify webservice added for testing")
+})
 app.use('/user', userRouter);
 app.use('/package', DestinationRouter);
 app.use('/book', BookingRouter);
 
 app.use(myErrorLogger);
+app.listen(PORT,()=>{
+    console.log(`Listening on ${ PORT }`);
+});
 
-app.listen(3000);
-console.log("Server listening in port 3000 ");
 
 module.exports = app;
